427d56f111304d08ff7adeb14cb4505a0154b40f
EugeneDashko <eugenedashko@yahoo.com>
2023-03-14 23:03:27 +0300

    feat: working on header 14.03.2023 23:03 PM

--- shelter/src/js/const.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = 'https://blue-juvenile-dime.glitch.me';
-export const PREFIX_PRODUCT = '/api/product';
--- shelter/src/js/getData.js
+++
@@ -1,6 +0,0 @@
-export const getData = async (url) => { // в modalOpen вызываем
-    const response = await fetch(url)  //.then (если не использовать async/await)
-    if (response.ok) {
-        return response.json() // json тоже асинхронный и  там куда мы его возвращаем (renderListProduct) нужно использовать then или await
-    }
-}
